98a25dcf9c3ff8df14b90098c2767e7e9f6fd14e
duanzx <15726645490@163.com>
2019-08-28 10:30:22 +0800

    test springCreateFactoriesInstances

--- src/test/java/cn/tutorial/initialize/SpringCreateFactoriesInstancesTest.java
+++ src/test/java/cn/tutorial/initialize/SpringCreateFactoriesInstancesTest.java
@@ -1,6 +1,10 @@
 package cn.tutorial.initialize;
 
 import org.junit.Test;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.ClassUtils;
+
+import java.lang.reflect.Constructor;
 
 public class SpringCreateFactoriesInstancesTest {
 // 参考 spring createSpringFactoriesInstances
@@ -20,12 +24,18 @@ public class SpringCreateFactoriesInstancesTest {
 //    }
 
     @Test
-    public void testCreateInstance(){
-
+    public void testCreateInstance()throws Exception {
+        Person person = createInstance(Person.class,"cn.tutorial.initialize.Person",new Class[]{String.class},new Object[]{"duanzx"});
+        System.out.println(person);
     }
 
-    private void createInstance(String className){
-
+    //todo  Assert.isAssignable ?
+    private <T> T createInstance(Class<T> type, String className, Class<?>[] parameterTypes, Object[] args) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Class<?> instanceClass = ClassUtils.forName(className, classLoader);
+        Constructor<?> constructor = instanceClass.getDeclaredConstructor(parameterTypes);
+        T instance = (T) BeanUtils.instantiateClass(constructor, args);
+        return instance;
     }
 
 }
